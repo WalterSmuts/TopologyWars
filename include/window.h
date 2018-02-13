5e9ba7df023b50c5dd0db40e46ae15fa592d8158
TheTravelingSpaceman <smuts.walter@gmail.com>
2018-02-13 17:27:51 +0200

    Block now moves on key-presses

--- include/window.h
+++ include/window.h
@@ -5,11 +5,23 @@
 #include <QPainter>
 #include <QPaintEvent>
 
+#define LEFT 16777234
+#define RIGHT 16777236
+#define UP 16777235
+#define DOWN 16777237
+#define EXIT 16777216
+
 class Window : public QWidget
 {
 	public:
 	explicit Window(QWidget *parent = 0);
-	void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+	void paintEvent(QPaintEvent *event);// Q_DECL_OVERRIDE; Not needed???
+	void keyPressEvent(QKeyEvent *event);
+
+	private:
+	//This is temp ship vars
+	QColor colour = Qt::red;
+	QRectF rect = QRectF(0, 0, 10, 10);
 };
 
 #endif // WINDOW_H
--- src/window.cpp
+++ src/window.cpp
@@ -1,4 +1,6 @@
 #include "window.h"
+#include <iostream>
+using namespace std;
 
 Window::Window(QWidget *parent) :
 	QWidget(parent)
@@ -15,9 +17,21 @@ void Window::paintEvent(QPaintEvent *event)
 	//Paint black background
 	painter.fillRect(event->rect(), Qt::black);
 	//Paint 10x10 red square in middle of screen
-	painter.fillRect(event->rect().width()/2-5, event->rect().height()/2-5, 10, 10, Qt::red);
+	painter.fillRect(this->rect, colour);
 
 	//Finalize Paint event
 	QWidget::paintEvent(event);
 	painter.end();
+}
+
+void Window::keyPressEvent(QKeyEvent *event){
+	cout << "KeyPress!!! " << event->key() << endl;
+	if (event->key() == LEFT) 	this->rect.moveLeft(this->rect.left()-10);
+	if (event->key() == RIGHT) 	this->rect.moveLeft(this->rect.left()+10);
+	if (event->key() == UP) 	this->rect.moveTop(this->rect.top()-10);
+	if (event->key() == DOWN) 	this->rect.moveTop(this->rect.top()+10);
+
+	if (event->key() == EXIT) this->close();
+
+	QWidget::update();
 }
